ee670993026ff5ff0d090bf8e1ebaa3613d7ca54
Korbos <brooks645@gmail.com>
2015-12-07 18:04:51 -0400

    Update InfoApp.java

--- CS2043Lab10/src/InfoApp.java
+++ CS2043Lab10/src/InfoApp.java
@@ -1,7 +1,10 @@
-public class InfoApp {
+ public class InfoApp {
+ 
+ 	public static void main(String[] args) {
+-		System.out.print("Team 3 member list:");
++		InfoPrintGoodwin emilynn = new InfoPrintGoodwin();
++		emilynn.printGoodwin();
 
-	public static void main(String[] args) {
-		System.out.print("Team 3 member list: ");
 		InfoPrintConklin Brooks = new InfoPrintConklin();
 		Brooks.printConklin();
 	}
