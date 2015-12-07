41f3e3d905954b2247e52ef0fd5958c80084fff4
Korbos <brooks645@gmail.com>
2015-12-07 18:08:29 -0400

    Create InfoPrintConklin.java

---
+++ CS2043Lab10/src/InfoPrintConklin.java
@@ -0,0 +1,18 @@
+/**
+ * @author bconklin
+ *
+ */
+public class InfoPrintConklin {
+
+	/**
+	 * @param args
+	 */
+	public void printConklin() {
+		
+		System.out.println("Brooks T. Conklin");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
